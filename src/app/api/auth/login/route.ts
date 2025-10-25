27d4df80a74f2cedd5697f8006b9505796958536
Alison Pariela <parielaalison@gmail.com>
2025-10-25 22:56:43 +0400

    fixed mistake in route file

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users/login`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
