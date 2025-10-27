5119618a17df509cf4ca5fd50bb2eb1ae204d0e9
Alison Pariela <parielaalison@gmail.com>
2025-10-27 12:46:00 +0400

    changed width

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,9 +4,9 @@ import CustomNavMenu from '@/components/navigation/navMenu';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="flex min-h-screen">
       <CustomNavMenu/>
-      <main className="flex-1 overflow-y-auto bg-white">
+      <main className="flex-1 md:ml-45 overflow-y-auto bg-white">
         {children}
       </main>
     </div>    
