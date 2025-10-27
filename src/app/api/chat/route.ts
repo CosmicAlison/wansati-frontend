2fcafdb7cce59d5eaf480581ee4cbbb1186629fd
Alison Pariela <parielaalison@gmail.com>
2025-10-27 09:28:13 +0400

    cleaned unused routes

--- src/app/api/chat/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getUserChatsServer } from "@/lib/server/ChatService";
-
-export async function GET() {
-  const chats = await getUserChatsServer();
-  return NextResponse.json(chats);
-}
